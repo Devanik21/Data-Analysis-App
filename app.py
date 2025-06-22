37306c44af0d8c1626502f9294dcf8a03c514c05
Devanik Debnath <162272415+Devanik21@users.noreply.github.com>
2025-06-22 20:53:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
-from plotly.subplots import make_subplots;;;;;;;;;;
+from plotly.subplots import make_subplotskkkkkkkkkkk
 import seaborn as sns
 import matplotlib.pyplot as plt
 import sqlite3
